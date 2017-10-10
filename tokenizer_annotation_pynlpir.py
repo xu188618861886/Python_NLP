ed6098e8b9380202cc7b4790a0a6c3249765b60b
徐嵩 <xu188618861886@163.com>
2017-10-10 11:14:44 +0800

    分词并词性标注，利用中科院pynlpir

--- tokenizer_annotation_pynlpir.py
+++ tokenizer_annotation_pynlpir.py
@@ -7,7 +7,7 @@ import pynlpir
 
 pynlpir.open()
 s = '聊天机器人到底该怎么做呢？'
-segments = pynlpir.segment(s)
+segments = pynlpir.segment(s,pos_english=False)
 for segment in segments:
     print (segment[0], '\t', segment[1])
 
